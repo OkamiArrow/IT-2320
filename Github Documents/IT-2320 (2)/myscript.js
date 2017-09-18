5a8e6a9ee54f1bcae0210a12e4bd54d02064fa6a
OkamiArrow <kiritoichigo1@gmail.com>
2017-09-17 23:43:43 -0400

    Javascript and Html revised

--- Github Documents/IT-2320 (2)/myscript.js
+++ Github Documents/IT-2320 (2)/myscript.js
@@ -1,34 +1,34 @@
 function imgChange(myImage3)
 {
-	myImage3.src = '../images/Itachi.jpg';
+	myImage3.src = './images/Itachi.jpg';
 	
 }
 
 function imgAway(myImage3)
 {
-	myImage3.src = '../images/NarutoLogo.jpg';
+	myImage3.src = './images/NarutoLogo.jpg';
 }
 
 function imgHover(myImage)
 {
-	myImage.src = '../images/Naruto2.jpg';
+	myImage.src = './images/Naruto2.jpg';
 	
 }
 
 function mouseAway(myImage)
 {
-	myImage.src = '../images/Naruto_characters.jpg';
+	myImage.src = './images/Naruto_characters.jpg';
 }
 
 
 function imgOn(myImage2)
 {
-	myImage2.src = '../images/NinaAlexander.jpg';	
+	myImage2.src = './images/NinaAlexander.jpg';	
 }
 
 function mouseOff(myImage2)
 {
-	myImage2.src = '../images/FullMetalAlchemist.jpg';
+	myImage2.src = './images/FullMetalAlchemist.jpg';
 }
 
 var timeObject = new Date();
