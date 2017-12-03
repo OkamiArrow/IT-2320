511a1387a257e4a78fb3bc8e098b87a44633f205
OkamiArrow <kiritoichigo1@gmail.com>
2017-12-03 18:19:47 -0500

    changes

--- Github Documents/IT-2320 (2)/Final Project Start/FinalProject/Scripts/Home.js
+++ Github Documents/IT-2320 (2)/Final Project Start/FinalProject/Scripts/Home.js
@@ -1,1 +1,12 @@
-﻿
+﻿$(document).ready(function () {
+
+    $('#form1').on('submit', function (e) {
+        e.preventDefault();
+        var details = $('#form1').serialize();
+
+        $.post('form1.php', details, function (data) {
+            $('#form1').html(data);
+        });
+    });
+
+});
